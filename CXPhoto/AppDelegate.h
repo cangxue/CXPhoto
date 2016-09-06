67775432bcc832e575d9374d9491764809cd5231
cangxue <752787086@qq.com>
2016-09-06 15:44:09 +0800

    Initial Commit

---
+++ CXPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CXPhoto
+//
+//  Created by xiaoma on 16/9/6.
+//  Copyright © 2016年 CX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CXPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CXPhoto
+//
+//  Created by xiaoma on 16/9/6.
+//  Copyright © 2016年 CX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
